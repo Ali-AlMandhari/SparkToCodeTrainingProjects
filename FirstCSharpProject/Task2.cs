6dc6b56c9b5fc084dcb53663278283eb8d60fbb8
Ali Al Mandhari <alishukri03@gmail.com>
2026-07-16 23:13:02 +0400

    wrote how to do the Sum of numbers 1 to N

---
+++ FirstCSharpProject/Task2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstCSharpProject
+{
+    internal class Task2
+    {
+        public void CalculateSum()
+        {
+            Console.WriteLine(" Sum of numbers 1 to N ");
+            Console.Write("Enter a positive whole number N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("The sum of numbers from 1 to {0} is {1}", n, sum);
+        }
+    }
+}
