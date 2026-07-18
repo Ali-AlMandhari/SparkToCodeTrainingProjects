fda8583acc2abe73f7e432100dad314d48a63a49
Ali Al Mandhari <alishukri03@gmail.com>
2026-07-18 21:22:44 +0400

    completed task 4

--- FirstCSharpProject/Task 4.cs
+++ FirstCSharpProject/Task 4.cs
@@ -6,5 +6,19 @@ namespace FirstCSharpProject
 {
     internal class Task_4
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Password Retry ");
+            string correctPassword = "Spark2026";
+            Console.Write("Enter the password: ");
+            String userGuess = Console.ReadLine();
+            while (userGuess != correctPassword)
+            {
+                Console.WriteLine("Incorrect password. Please try again.");
+                Console.Write("Enter the password: ");
+                userGuess = Console.ReadLine();
+            }
+            Console.WriteLine("Password correct. Access granted.");
+        }
     }
 }
