c88965f48efaf28029bf093223162ce83e6845f3
Ali Al Mandhari <alishukri03@gmail.com>
2026-07-18 20:04:47 +0400

    asking for user to write a number

---
+++ FirstCSharpProject/Task 3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstCSharpProject
+{
+    internal class Task_3
+    {
+        Console.WriteLine(" Multiplication Table ");
+    }
+}
