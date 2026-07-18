f3b529518a4b915d080ed0b36e1ebdeac7f18957
Ali Al Mandhari <alishukri03@gmail.com>
2026-07-18 20:58:11 +0400

    finished the task

---
+++ FirstCSharpProject/Task 3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstCSharpProject
+{
+    internal class Task_3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Multiplication Table ");
+            Console.Write(" Enter a number to see its multiplication table");
+            int tableNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = tableNumber * i;
+                Console.WriteLine(tableNumber + " x " + i + " = " + result);
+            }
+        }
+    }
+}
